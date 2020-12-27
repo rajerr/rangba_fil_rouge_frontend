2feb07da9671f0fa0185dc455003be641b5dd08f
rajerr <rajerr2013@gmail.com>
2020-12-27 13:58:38 +0000

    --- Auto-Git Commit ---

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
+import { UsersComponent } from './users/users.component';
+import { RendusComponent } from './rendus/rendus.component';
+import { ReferentielsComponent } from './referentiels/referentiels.component';
+import { PromosComponent } from './promos/promos.component';
+import { ProfilesSortieComponent } from './profiles-sortie/profiles-sortie.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { GroupeTagsComponent } from './groupe-tags/groupe-tags.component';
+import { GroupeCompetencesComponent } from './groupe-competences/groupe-competences.component';
+import { ForumsComponent } from './forums/forums.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { BriefsComponent } from './briefs/briefs.component';
 import { FoundComponent } from './found/found.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
@@ -6,8 +17,22 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path:'', redirectTo:'/login', pathMatch:'full'},
   {path:'home', component: HomeComponent},
+  {path:'users', component:UsersComponent},
+  {path:'promos', component:PromosComponent},
+  {path:'rendus', component:RendusComponent},
+  {path:'briefs', component:BriefsComponent},
+  {path:'forums', component:ForumsComponent},
+  {path:'profiles', component:ProfilesComponent},
+  {path:'competences', component:CompetencesComponent},
+  {path:'groupes-tags', component:GroupeTagsComponent},
+  {path:'referentiels', component:ReferentielsComponent},
+  {path:'profiles-sortie', component:ProfilesSortieComponent},
+  {path:'goupes-competences', component:GroupeCompetencesComponent},
+
+
   {path:'page-not-found', component:FoundComponent},
-  {path:'**', redirectTo:'/page-not-found'}
+  {path:'**', redirectTo:'/page-not-found'},
+
 ];
 
 @NgModule({
