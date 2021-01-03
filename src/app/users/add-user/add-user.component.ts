62c674f5448e3d62d271f4a6b14ba7861c624db0
rajerr <rajerr2013@gmail.com>
2021-01-03 14:38:59 +0000

    --- Auto-Git Commit ---

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsersService } from './../../services/users.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
+  addUserForm : FormGroup | any;
+  nom: string |any;
+  prenom: string |any;
+  email: string |any;
+  profile: string |any;
   constructor(
     private usersService: UsersService,
     private router : Router,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.addUserForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      email: ['', Validators.required],
+      profile: ['', Validators.required],
+    })
+  }
+
+
+  onSubmit(){
+    if (this.addUserForm.invalid) {
+      return this.addUserForm.value;
+    }
+
+    this.usersService.addUsers(this.addUserForm.value)
+    console.log(this.addUserForm.value)
   }
 
 }
