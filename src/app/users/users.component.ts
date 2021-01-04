41c2376fe814fdde4bc9b4566ca4009994b23409
rajerr <rajerr2013@gmail.com>
2021-01-04 08:15:15 +0000

    --- Auto-Git Commit ---

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { UsersService } from './../services/users.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthentificationService } from '../services/authentification.service';
 
 @Component({
@@ -30,5 +30,15 @@ export class UsersComponent implements OnInit {
     }
     
   }
+
+
+  @HostListener('scroll', ['$event'])
+onScroll(event: any) {
+    // visible height + pixel scrolled >= total height 
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+      console.log("End");
+    }
+}
+
 
 }
